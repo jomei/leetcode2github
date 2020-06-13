05449843d3029611cfe7bb72d6f4ba184f5fec30
Anatoy Nosov <jomeisama@gmail.com>
2020-06-13 14:02:56 +0700

    #4 Generate solution file name

--- src/popup/SolutionForm.tsx
+++ src/popup/SolutionForm.tsx
@@ -26,7 +26,7 @@ export default class SolutionForm extends Component<SolutionProps, SolutionState
         this.state = {
             commit: {
                 repo: "",
-                fileName: this.titleToFileName(props.solution.title),
+                fileName: generateFileName(props.solution.title, props.solution.lang),
                 message: props.solution.title,
                 content: props.solution.source
             },
@@ -116,8 +116,32 @@ export default class SolutionForm extends Component<SolutionProps, SolutionState
     onBackClick() {
         this.setState({showSuccess: false, showError: false})
     }
+}
 
-    titleToFileName(title: string): string {
-        return title.toLowerCase().replace(" ", "_")
+function generateFileName(problemTitle: string, lang: string): string {
+    if (problemTitle == "") {
+        return ""
     }
+    const title = problemTitle.toLowerCase().replace(".", "").replace(" ", "_")
+    return `${title}.${getExtensionByLang(lang)}`
+}
+
+const LANG_MAP = {
+    "python": "py",
+    "python3": "py",
+    "c++": "cpp",
+    "c#": "cs",
+    "javascript": "js",
+    "ruby": "rb",
+    "rust": "rs",
+    "typescript": "ts",
+    "scala": "sc",
+}
+
+function getExtensionByLang(lang: string): string {
+    const l = lang.toLowerCase()
+    if(l in LANG_MAP) {
+        return LANG_MAP[l]
+    }
+    return l
 }
